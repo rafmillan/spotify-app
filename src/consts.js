00ac1f68add6e37ca70146f3b4554fdc25b827b6
rafmillan <rafael.k.millan@gmail.com>
2023-02-26 23:39:07 -0800

    added constants file

---
+++ src/consts.js
@@ -0,0 +1,8 @@
+export const CLIENT_ID = "6473932552584932a3ec9920a5d48230"
+export const REDIRECT_URI = "http://localhost:3000"
+export const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+export const RESPONSE_TYPE = "token"
+
+export const USER_TOP_READ = "user-top-read"
+
+export const BASE_URL = 'https://api.spotify.com/v1';
